46cde485e00ed13c145109faffc4702b23b33431
Dilip-36 <dilipnava2@example.com>
2023-10-18 17:22:03 +0530

    first commit

---
+++ Codingrajatech-Music player app pro-Diliptask2/js/music-list.js
@@ -0,0 +1,140 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "DJ Snake - Taki Taki",
+    artist: "DJ Snake ft.Cardi B, Selene Gomez, Ozuna",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Alan Walker - Faded",
+    artist: "Alan Walker",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Alan Walker - Darkside",
+    artist: "Alan Walker ft.Au/Ra & Tomine Harket",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Eminem - Mockingbird",
+    artist: "Eminem",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Coolio - Gangsta's Paradise",
+    artist: "Coolio ft.L.V",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Imagine Dragons - Thunder",
+    artist: "Imagine Dragons",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Ruth B. - Dandelions",
+    artist: "Ruth B.",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Sickick - Infected",
+    artist: "Sickick",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Rosa Linn - SNAP",
+    artist: "Rosa Linn",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Lost Sky - Fearless Pt II",
+    artist: "Lost Sky ft.Chris Linton",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Westlife - I had a dream",
+    artist: "Westlife",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "K'naan - Wavin' Flag",
+    artist: "K'naan",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Joel Adams - Please Don't Go",
+    artist: "Joel Adams",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Jaymes Young - Infinity",
+    artist: "Jaymes Young",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Maroon 5 - Memories",
+    artist: "Maroon 5",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Why mona - Wannabe",
+    artist: "Why mona",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "XXXTentacion - Hope",
+    artist: "XXXTentacion",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "The Script - Hall Of Fame",
+    artist: "The Script",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "The Weeknd - Blinding Lights",
+    artist: "The Weeknd",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "The Weeknd - Starboy",
+    artist: "The Weeknd ft.Daft Punk",
+    img: "music-20",
+    src: "music-20"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
